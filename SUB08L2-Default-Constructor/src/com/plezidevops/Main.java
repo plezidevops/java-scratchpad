09541237031b4c8060147f9a5c6fdc713cc5b51e
pascalmlaurent@gmail.com <plaurent@pascal.jeremieonline.com>
2022-02-22 18:23:07 -0500

    default constructor

---
+++ SUB08L2-Default-Constructor/src/com/plezidevops/Main.java
@@ -0,0 +1,12 @@
+package com.plezidevops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SmartPhone phone = new SmartPhone();
+		
+		System.out.println(phone.getBrand());
+
+	}
+
+}
---
+++ SUB08L2-Default-Constructor/src/com/plezidevops/SmartPhone.java
@@ -0,0 +1,11 @@
+package com.plezidevops;
+
+public class SmartPhone {
+private String brand = "Samsung";
+
+public String getBrand() {
+	return brand;
+}
+
+
+}
