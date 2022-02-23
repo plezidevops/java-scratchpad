8b9162ac24c084f75ccb5b01051ceb08463e1b49
pascalmlaurent@gmail.com <plaurent@pascal.jeremieonline.com>
2022-02-23 10:29:30 -0500

    usage of constructor

---
+++ SUB08L3-Usage-of-Constructor/src/com/plezidevops/Car.java
@@ -0,0 +1,47 @@
+package com.plezidevops;
+
+public class Car {
+	// properties
+	private String doors;
+	private String engine;
+	private String driver;
+	private int speed;
+	
+	
+	public Car() {
+		this("closed", "on", "seated", 100);
+	}
+	
+	public Car(String doors, String engine, String driver, int speed) {
+		this.doors = doors;
+		this.engine = engine;
+		this.driver = driver;
+		this.speed = speed;
+	}
+		
+	public int getSpeed() {
+		return this.speed;
+	}
+
+	public String getDoors() {
+		return doors;
+	}
+
+	public String getEngine() {
+		return engine;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public String run() {
+		if (this.doors.equals("closed") && this.engine.equals("on") && 
+				this.driver.equals("seated") && this.speed > 0) {
+			return "running";
+		}
+		return "not running";
+	}
+
+}
+
